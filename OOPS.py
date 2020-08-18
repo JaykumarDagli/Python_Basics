de69fe75a99197f953c5fa6b8286e4be84de8e83
JaykumarDagli <43545853+JaykumarDagli@users.noreply.github.com>
2020-08-18 16:06:30 +0530

    updated with inheritance, abstract class

--- OOPS.py
+++ OOPS.py
@@ -7,8 +7,8 @@ Created on Mon Aug 17 11:11:52 2020
 
 """ CLASS & OBJECT """
 
-#Class: Blueprint to create new object, everything is an object in Python eg: Animal
-#Obj: Instance of a class eg: Cat, they have attributes and methods
+# Class: Blueprint to create new object, everything is an object in Python eg: Animal
+# Obj: Instance of a class eg: Cat, they have attributes and methods
 
 class Point:
     def draw(self):
@@ -19,9 +19,9 @@ point.draw()
 
 print(isinstance(point, Point)) #helps to know if obj is from the specified class
 
-#CONSTRUCTOR: method called when creating new object
+# CONSTRUCTOR: method called when creating new object
 
-#'self' is a reference to current object
+# 'self' is a reference to current object
 
 class Point:
     
@@ -83,6 +83,75 @@ print('Comparison is: ', p1==p2)
 #there are magic methods to solve this eg: __eq__
 #and there are methods for arithmetic operators as well
 
+""" INHERITANCE """
+
+# mechanism to allow common functions in one class and inherit them in
+# other classes
+
+class Animal:
+    
+    #can inherit variables/attributes
+    def __init__(self):
+        print('Animal Constructor')
+        self.age = 1
+    
+    #can inherit functions 
+    def eat(self):
+        print('Eat')
+
+#'Animal' is parent and 'mammal' is child class
+
+#suppose if we have constructor in the child, it'll override 
+#the one in the base class and thus, we have to write 'super' 
+#in doing so, first the constructor of the parent class is called 
+#and then of child class. If this is not done, we get 'AttributeError'
+
+
+class Mammal(Animal):
+    
+    def __init__(self):
+        super().__init__() 
+        print('Mammal Constructor')
+        self.wt = 1
+    
+    def walk(self):
+        print('Walk')
+        
+m1=Mammal()
+print(m1.age)
+
+""" ABSTRACT CLASS """
+
+# Sometimes the parent class is only needed to provide inheritance 
+# eg: we can have a base class a human, and the child classes as 
+# male and female but 'human' will never be needed for the code i.e.
+# we'll create objects only from the child classes and in this case
+# the parent class should be made 'abstract' 
+
+# 'abc' stands for Abstract Base Class
+
+from abc import ABC, abstractmethod
+
+class Human(ABC):
+    
+    @abstractmethod    
+    def smile(self):
+        print('Smile')
+
+class Male(Human):
+    
+    def taska(self):
+        pass
+    
+class Female(Human):
+    
+    def taskb(self):
+        pass
+    
+# once this is done, we cannot create objects from the 'human' class
+# and it becomes abstract
+    
+""" POLYMORPHISM """
 
 
 
@@ -103,3 +172,9 @@ print('Comparison is: ', p1==p2)
 
 
 
+
+
+
+
+
+    
